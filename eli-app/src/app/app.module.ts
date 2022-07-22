f4f91a3bd307fbedef0290c2c1d6fa2ffbc2ea57
elianyrobles <martinezeliany1996@gmail.com>
2022-07-22 17:02:56 -0400

    remove sidenav

--- eli-app/src/app/app.module.ts
+++ eli-app/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SidenavComponent } from './core/containers/sidenav/sidenav.component';
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidenavComponent,
   ],
   imports: [
     BrowserModule,
--- eli-app/src/app/core/core.module.ts
+++ eli-app/src/app/core/core.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 
-
-
 @NgModule({
   declarations: [
     
